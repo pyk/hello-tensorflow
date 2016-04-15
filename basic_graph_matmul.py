f168178b43a12ded6616397f6aee2c4b018f4f59
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-04-15 15:59:35 +0000

    execute three ops in the default graph

--- basic_graph_matmul.py
+++ basic_graph_matmul.py
@@ -1,8 +1,14 @@
 import tensorflow as tf
 
 # matrix 1x2
-matrix1 = tf.Constant([[3., 3.]])
+matrix1 = tf.constant([[3., 3.]])
 # matrix 2x1
-matrix2 = tf.Constant([[2.], [2.]])
+matrix2 = tf.constant([[2.], [2.]])
 # matrix multiplication
 product = tf.matmul(matrix1, matrix2)
+
+# launch session on default graph
+sess = tf.Session()
+result = sess.run(product)
+print result
+sess.close()
